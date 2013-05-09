6d4ac3692d041c2985fb4c33238a9568ff9fea6a
jeroenrietveld <jeroen_rietveld13@msn.com>
2013-05-09 22:34:25 +0200

    little more cleaning up and added a database config file

--- bootstrap.php
+++ bootstrap.php
@@ -1,16 +1,12 @@
 <?php
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\Yaml\Yaml;
 
 require_once 'vendor/autoload.php';
 
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), false);
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src/entities"), false);
 
-$dbConfiguration = array(
-	'driver'   => 'pdo_mysql',
-	'user'	   => 'root',
-	'password' => 'root',
-	'dbname'   => 'restapi',
-);
+$dbSettings = Yaml::parse(file_get_contents('./config/database.yml'));
 
-$em = EntityManager::create($dbConfiguration, $config);
+$em = EntityManager::create($dbSettings, $config);
